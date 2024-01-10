55d0fb56b8a6b38477b0b4a0956e24a8d5dc3eb7
Curtis M <curtis11macleod@gmail.com>
2024-01-10 11:05:26 -0500

    Added keyboard input for the tank

---
+++ Assets/Week 1/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float keyboardInput = Input.GetAxis("Horizontal");
+        transform.Translate(keyboardInput * speed * Time.deltaTime, 0, 0);
+    }
+}
