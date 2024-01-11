a29653b8dd60ceb822fd39d770bf2d68437ec57f
Curtis <macleocu@shernet.sheridancollege.ca>
2024-01-10 20:34:53 -0500

    Fix merge conflicts

--- Assets/Week 1/Scripts/Rotator.cs
+++ Assets/Week 1/Scripts/Rotator.cs
@@ -9,10 +9,7 @@ public class Rotator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-        
-=======
->>>>>>> Week1-Task3
+
     }
 
     // Update is called once per frame
